a59d6b80557ce6665c5258cc7dd6cdd3ee025dfb
smaillim-p <smaillim.paz@ubs.com>
2022-11-14 14:46:50 +0100

    Initial Commit

---
+++ UIKit_in_SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UIKit_in_SwiftUI_Sample
+//
+//  Created by Smaillim Paz on 14.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIKit_in_SwiftUI_Sample/UIKit_in_SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_in_SwiftUI_SampleApp.swift
+//  UIKit_in_SwiftUI_Sample
+//
+//  Created by Smaillim Paz on 14.11.22.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_in_SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
